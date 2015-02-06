b55f9f7082d600d82c56b00b5a7cd3884bec8f6e
garnetlithos <garnetlithos@gmail.com>
2015-02-07 03:05:27 +0900

    add find answer logic in while loop // find answer in rows_pool

--- picturelogic_find_answer.py
+++ picturelogic_find_answer.py
@@ -18,11 +18,45 @@ def picturelogic_find_answer(data):
         cycle_max.append(len(row_pool))
 
     while True:
-        print(cycles)
-        cycles = cycle_count(cycles, cycle_max)
-        if cycles == -1:
-            print("End cycles, no answer.")
+        break_flag = 0
+
+        for col_line in range(len(data['cols'])):
+            test_col = [0]
+
+            for i in reversed(range(len(data['rows']))):
+                if rows_pool[i][cycles[i]][col_line] == 0:
+                    if i == len(data['rows']) - 1:
+                        pass
+                    elif rows_pool[i+1][cycles[i+1]][col_line] == 1:
+                        test_col.append(0)
+                    elif rows_pool[i][cycles[i]][col_line] == 0:
+                        pass
+                    else:
+                        print("make test col error")
+                elif rows_pool[i][cycles[i]][col_line] == 1:
+                    test_col[-1] += 1
+                else:
+                    print("make test col error")
+            else:
+                if test_col[-1] == 0 and len(test_col) != 0:
+                    test_col.pop(-1)
+
+            if test_col == data['cols'][col_line]:
+                pass
+            else:
+                break
+        else:
+            print("find answer")
+            print(cycles)
+            break_flag = 1
+
+        if break_flag == 1:
             break
+        else:
+            cycles = cycle_count(cycles, cycle_max)
+            if cycles == -1:
+                print("End cycles, no answer.")
+                break
 
 
 def cycle_count(cycles, cycle_max):
@@ -35,11 +69,7 @@ def cycle_count(cycles, cycle_max):
         if cycles[i] == cycle_max[i]:
             cycles[i] = 0
             cycles[i+1] += 1
-            # print(cycles)
 
     return cycles
-
-
-
 
 picturelogic_find_answer(data)
